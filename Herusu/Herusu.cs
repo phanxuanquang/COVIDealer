7048db0adba76d54490c09cab5f7489e833620b2
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-03-17 00:33:30 +0700

    Add project files.

---
+++ Herusu/Herusu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herusu
+{
+    public partial class Herusu : Form
+    {
+        public Herusu()
+        {
+            InitializeComponent();
+        }
+    }
+}

07660675c1bc1cfd33ea9ea763a0d036b4f1fd58
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-04-26 21:21:00 +0700

    API can work now

--- Herusu/Herusu.cs
+++ Herusu/Herusu.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Herusu
 {
@@ -15,12 +22,48 @@ namespace Herusu
         public Herusu()
         {
             InitializeComponent();
-        }
 
+        }
+        bool first = true;
         private void SendButton_Click(object sender, EventArgs e)
         {
-            ChatSpace.Text += InputTextBox.Text + "\n";
-            InputTextBox.Text = String.Empty;
+            //var YOUR_API_KEY = "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"; 
+            //var userInput = InputTextBox.Text;
+            //ChatSpace.AppendText("User: " + userInput + Environment.NewLine);
+            //var client = new RestClient("https://api.openai.com/v1");
+            //var request = new RestRequest("engines/text-davinci-003/completions", Method.Post);
+            //request.AddHeader("Content-Type", "application/json");
+            //request.AddHeader("Authorization", $"Bearer {YOUR_API_KEY}");
+            //request.AddJsonBody(new { prompt = userInput, max_tokens = 4000, temperature = 0 });
+            //var response = client.Execute(request);
+            //var responseData = JObject.Parse(response.Content);
+            //string output = responseData["choices"][0]["text"].ToString();
+            //ChatSpace.AppendText("OpenAI: " + output + Environment.NewLine);
+            //InputTextBox.Text = String.Empty;
+            string input = InputTextBox.Text;
+            showRequestToChatSpace();
+            string apiKey = "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";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + input + "\", \"temperature\": 0, \"max_tokens\":4000}", Encoding.UTF8, "application/json");
+            var response = client.PostAsync("https://api.openai.com/v1/completions", content).Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            dynamic responseJson = JsonConvert.DeserializeObject(responseString);
+            string completion = responseJson.choices[0].text;
+            ChatSpace.Text += completion + "\n";
+            
+        }                                                                                      
+
+        string getRespond(string request)
+        {
+            string answer = "Nothing";
+            return String.Format("Herusu: {0} \n", answer);
+        }
+
+        void showRequestToChatSpace()
+        {
+           ChatSpace.Text += String.Format("Client: {0} \nHerusu: ", InputTextBox.Text);
+           InputTextBox.Text = String.Empty;
         }
     }
 }

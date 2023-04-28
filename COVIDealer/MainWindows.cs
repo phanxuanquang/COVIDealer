930fac87ba3a66cb2a3c0452fc020ceece70e807
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-04-28 14:34:33 +0700

    test

--- COVIDealer/MainWindows.cs
+++ COVIDealer/MainWindows.cs
@@ -66,7 +66,7 @@ namespace COVIDealer
         {
             HttpClient httpClient = new HttpClient();
             var response = await httpClient.GetAsync("https://static.pipezero.com/covid/data.json");
-            data = JsonConvert.DeserializeObject<Data>(await response.Content.ReadAsStringAsync());     
+            data = JsonConvert.DeserializeObject<Data>(await response.Content.ReadAsStringAsync());      
             statisticTab = new StatisticTab(data);
         }
     }

27f89529dee462f38c8a7d7539420da0cf042b2d
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-07-06 01:15:35 +0700

    fix dize

--- COVIDealer/News/ArticleThumbnail.Designer.cs
+++ COVIDealer/News/ArticleThumbnail.Designer.cs
@@ -30,50 +30,68 @@
         {
             this.Summary = new System.Windows.Forms.RichTextBox();
             this.contentPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.Tittle = new System.Windows.Forms.RichTextBox();
             this.ThumbnailImage = new System.Windows.Forms.PictureBox();
             this.contentPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ThumbnailImage)).BeginInit();
             this.SuspendLayout();
             // 
             // Summary
             // 
-            this.Summary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Summary.BackColor = System.Drawing.Color.White;
             this.Summary.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Summary.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Summary.Dock = System.Windows.Forms.DockStyle.Top;
             this.Summary.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.Summary.ForeColor = System.Drawing.Color.Gray;
-            this.Summary.Location = new System.Drawing.Point(270, 86);
+            this.Summary.Location = new System.Drawing.Point(0, 118);
+            this.Summary.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Summary.Name = "Summary";
             this.Summary.ReadOnly = true;
-            this.Summary.Size = new System.Drawing.Size(429, 74);
+            this.Summary.Size = new System.Drawing.Size(587, 158);
             this.Summary.TabIndex = 15;
             this.Summary.Text = "Ca Mắc COVID-19 Tăng, Bình Dương Yêu Cầu 6 Trường Hợp Phải Đeo Khẩu Trang | SKĐS";
             // 
             // contentPanel
             // 
+            this.contentPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.contentPanel.BackColor = System.Drawing.Color.White;
-            this.contentPanel.Controls.Add(this.Summary);
-            this.contentPanel.Controls.Add(this.Tittle);
+            this.contentPanel.Controls.Add(this.panel1);
             this.contentPanel.Controls.Add(this.ThumbnailImage);
             this.contentPanel.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.contentPanel.Location = new System.Drawing.Point(5, 5);
+            this.contentPanel.Location = new System.Drawing.Point(8, 8);
+            this.contentPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.contentPanel.Name = "contentPanel";
-            this.contentPanel.Size = new System.Drawing.Size(700, 162);
+            this.contentPanel.Size = new System.Drawing.Size(984, 276);
             this.contentPanel.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.Summary);
+            this.panel1.Controls.Add(this.Tittle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(397, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(587, 276);
+            this.panel1.TabIndex = 16;
             // 
             // Tittle
             // 
-            this.Tittle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Tittle.BackColor = System.Drawing.Color.White;
             this.Tittle.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Tittle.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Tittle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.Tittle.Location = new System.Drawing.Point(268, 30);
+            this.Tittle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Tittle.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Tittle.Location = new System.Drawing.Point(0, 0);
+            this.Tittle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Tittle.Name = "Tittle";
             this.Tittle.ReadOnly = true;
-            this.Tittle.Size = new System.Drawing.Size(432, 55);
+            this.Tittle.Size = new System.Drawing.Size(587, 118);
             this.Tittle.TabIndex = 12;
             this.Tittle.Text = "Ca Mắc COVID-19 Tăng, Bình Dương Yêu Cầu 6 Trường Hợp Phải Đeo Khẩu Trang | SKĐS";
             this.Tittle.Click += new System.EventHandler(this.Tittle_Click);
@@ -85,22 +103,26 @@
             this.ThumbnailImage.BackColor = System.Drawing.Color.Transparent;
             this.ThumbnailImage.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ThumbnailImage.Location = new System.Drawing.Point(0, 0);
+            this.ThumbnailImage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ThumbnailImage.Name = "ThumbnailImage";
-            this.ThumbnailImage.Size = new System.Drawing.Size(260, 162);
+            this.ThumbnailImage.Size = new System.Drawing.Size(390, 276);
+            this.ThumbnailImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ThumbnailImage.TabIndex = 11;
             this.ThumbnailImage.TabStop = false;
             // 
             // ArticleThumbnail
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.contentPanel);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ArticleThumbnail";
-            this.Size = new System.Drawing.Size(711, 172);
+            this.Size = new System.Drawing.Size(1000, 292);
             this.MouseEnter += new System.EventHandler(this.ArticleThumbnail_MouseEnter);
             this.MouseLeave += new System.EventHandler(this.ArticleThumbnail_MouseLeave);
             this.contentPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ThumbnailImage)).EndInit();
             this.ResumeLayout(false);
 
@@ -112,5 +134,6 @@
         private System.Windows.Forms.Panel contentPanel;
         private System.Windows.Forms.RichTextBox Tittle;
         private System.Windows.Forms.PictureBox ThumbnailImage;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- COVIDealer/Video/UI/VideoThumbnail.Designer.cs
+++ COVIDealer/Video/UI/VideoThumbnail.Designer.cs
@@ -30,16 +30,18 @@
         {
             this.contentPanel = new System.Windows.Forms.Panel();
             this.Summary = new System.Windows.Forms.RichTextBox();
+            this.UploadDate = new System.Windows.Forms.Label();
+            this.Channel = new System.Windows.Forms.Label();
             this.Tittle = new System.Windows.Forms.RichTextBox();
             this.ThumbnailImage = new System.Windows.Forms.PictureBox();
-            this.Channel = new System.Windows.Forms.Label();
-            this.UploadDate = new System.Windows.Forms.Label();
             this.contentPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ThumbnailImage)).BeginInit();
             this.SuspendLayout();
             // 
             // contentPanel
             // 
+            this.contentPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.contentPanel.BackColor = System.Drawing.Color.White;
             this.contentPanel.Controls.Add(this.Summary);
             this.contentPanel.Controls.Add(this.UploadDate);
@@ -47,41 +49,78 @@
             this.contentPanel.Controls.Add(this.Tittle);
             this.contentPanel.Controls.Add(this.ThumbnailImage);
             this.contentPanel.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.contentPanel.Location = new System.Drawing.Point(5, 5);
+            this.contentPanel.Location = new System.Drawing.Point(8, 8);
+            this.contentPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.contentPanel.Name = "contentPanel";
-            this.contentPanel.Size = new System.Drawing.Size(757, 180);
+            this.contentPanel.Size = new System.Drawing.Size(987, 277);
             this.contentPanel.TabIndex = 0;
             this.contentPanel.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
             this.contentPanel.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
             // 
             // Summary
             // 
-            this.Summary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Summary.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.Summary.BackColor = System.Drawing.Color.White;
             this.Summary.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Summary.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Summary.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.Summary.ForeColor = System.Drawing.Color.Gray;
-            this.Summary.Location = new System.Drawing.Point(327, 111);
+            this.Summary.Location = new System.Drawing.Point(490, 171);
+            this.Summary.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Summary.Name = "Summary";
             this.Summary.ReadOnly = true;
-            this.Summary.Size = new System.Drawing.Size(429, 69);
+            this.Summary.Size = new System.Drawing.Size(495, 106);
             this.Summary.TabIndex = 15;
             this.Summary.Text = "Ca Mắc COVID-19 Tăng, Bình Dương Yêu Cầu 6 Trường Hợp Phải Đeo Khẩu Trang | SKĐS";
             this.Summary.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
             this.Summary.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
             // 
+            // UploadDate
+            // 
+            this.UploadDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UploadDate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UploadDate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.UploadDate.ForeColor = System.Drawing.Color.Gray;
+            this.UploadDate.Location = new System.Drawing.Point(488, 92);
+            this.UploadDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UploadDate.Name = "UploadDate";
+            this.UploadDate.Size = new System.Drawing.Size(499, 35);
+            this.UploadDate.TabIndex = 14;
+            this.UploadDate.Text = "Upload Date";
+            this.UploadDate.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
+            this.UploadDate.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
+            // 
+            // Channel
+            // 
+            this.Channel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Channel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Channel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Channel.ForeColor = System.Drawing.Color.Gray;
+            this.Channel.Location = new System.Drawing.Point(488, 131);
+            this.Channel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Channel.Name = "Channel";
+            this.Channel.Size = new System.Drawing.Size(499, 35);
+            this.Channel.TabIndex = 13;
+            this.Channel.Text = "Channel";
+            this.Channel.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
+            this.Channel.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
+            // 
             // Tittle
             // 
-            this.Tittle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tittle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.Tittle.BackColor = System.Drawing.Color.White;
             this.Tittle.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Tittle.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Tittle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.Tittle.Location = new System.Drawing.Point(325, 2);
+            this.Tittle.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Tittle.Location = new System.Drawing.Point(488, 3);
+            this.Tittle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Tittle.Name = "Tittle";
             this.Tittle.ReadOnly = true;
-            this.Tittle.Size = new System.Drawing.Size(432, 55);
+            this.Tittle.Size = new System.Drawing.Size(499, 85);
             this.Tittle.TabIndex = 12;
             this.Tittle.Text = "Ca Mắc COVID-19 Tăng, Bình Dương Yêu Cầu 6 Trường Hợp Phải Đeo Khẩu Trang | SKĐS";
             this.Tittle.Click += new System.EventHandler(this.Tittle_Click);
@@ -94,51 +133,26 @@
             this.ThumbnailImage.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ThumbnailImage.Dock = System.Windows.Forms.DockStyle.Left;
             this.ThumbnailImage.Location = new System.Drawing.Point(0, 0);
+            this.ThumbnailImage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ThumbnailImage.Name = "ThumbnailImage";
-            this.ThumbnailImage.Size = new System.Drawing.Size(320, 180);
+            this.ThumbnailImage.Size = new System.Drawing.Size(480, 277);
+            this.ThumbnailImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ThumbnailImage.TabIndex = 11;
             this.ThumbnailImage.TabStop = false;
             this.ThumbnailImage.Click += new System.EventHandler(this.Tittle_Click);
             this.ThumbnailImage.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
             this.ThumbnailImage.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
             // 
-            // Channel
-            // 
-            this.Channel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Channel.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Channel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.Channel.ForeColor = System.Drawing.Color.Gray;
-            this.Channel.Location = new System.Drawing.Point(325, 85);
-            this.Channel.Name = "Channel";
-            this.Channel.Size = new System.Drawing.Size(432, 23);
-            this.Channel.TabIndex = 13;
-            this.Channel.Text = "Channel";
-            this.Channel.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
-            this.Channel.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
-            // 
-            // UploadDate
-            // 
-            this.UploadDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.UploadDate.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.UploadDate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.UploadDate.ForeColor = System.Drawing.Color.Gray;
-            this.UploadDate.Location = new System.Drawing.Point(325, 60);
-            this.UploadDate.Name = "UploadDate";
-            this.UploadDate.Size = new System.Drawing.Size(432, 23);
-            this.UploadDate.TabIndex = 14;
-            this.UploadDate.Text = "Upload Date";
-            this.UploadDate.MouseEnter += new System.EventHandler(this.contentPanel_MouseEnter);
-            this.UploadDate.MouseLeave += new System.EventHandler(this.contentPanel_MouseLeave);
-            // 
             // VideoThumbnail
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.contentPanel);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "VideoThumbnail";
-            this.Size = new System.Drawing.Size(766, 190);
+            this.Size = new System.Drawing.Size(1000, 292);
             this.contentPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ThumbnailImage)).EndInit();
             this.ResumeLayout(false);
